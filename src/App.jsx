c5eeacdf0476d9cda5a6580680d3d9df88c221ef
pranmandale <pranavmandale2025@gmail.com>
2024-11-09 19:34:33 +0530

    started

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-4xl text-center font-bold text-green-600 mt-20'>Word to Pdf Converter</h1>     
+    </>
+  )
+}
+
+export default App
